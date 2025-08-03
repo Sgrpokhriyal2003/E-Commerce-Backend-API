8990af010b4b667c4446fd2f9fd81c05a7c7bc53
Sgrpokhriyal2003 <sgrpokhriyal@gmail.com>
2025-08-03 11:16:41 +0530

    added mongodb connection

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 import express from "express";
 import dotenv from 'dotenv'
-dotenv.config()
+import morgan from 'morgan'
+import fileUpload from 'express-fileupload'
+import cookieParser from 'cookie-parser'
 
+//user defined imports
+import { connectDB } from "./src/config/db.js";
+
+
+dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan("dev"))
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/"
+}))
+
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome To E-Commerce Backend API 💸"
+    })
+})
 
 
 
 app.listen(PORT, () => {
     console.log(`server is listen on http://localhost:${PORT}`)
+    connectDB()
 })
 
