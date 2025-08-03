cfaf18a09d83225a1f313c6fe6538f18167dd346
Sgrpokhriyal2003 <sgrpokhriyal@gmail.com>
2025-08-03 12:33:12 +0530

    added auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,68 @@
+import User from '../models/user.model.js'
+import jwt from 'jsonwebtoken'
+
+export const protectRoute = async(req, res, next) => {
+    try{
+        const accessToken = req.cookies.accessToken
+        if(!accessToken){
+            return res.status(401).json({
+                success: false,
+                message: 'unauthorized! - no access token is provided'
+            })
+        }
+
+
+        try{
+            const decoded = jwt.verify(accessToken, process.env.JWT_ACCESSTOKEN)
+            const user = await User.findById(decoded.userId).select('-password')
+            if(!user){
+                return res.status(401).json({
+                    success: false,
+                    message: "user not found! incorrect user or accesstoken"
+                })
+            }
+
+            req.user = user
+            next()
+        }
+        catch(error){
+            if(error.name === "TokenExpiredError"){
+                return res.status(401).json({
+                    success: false,
+                    message: "unauthorized - access token expired"
+                })
+            }
+
+            throw error
+        }
+
+    }
+    catch(error){
+        console.log(`error in protect route middleware: `, error.message)
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized - invalid access token"
+        })   
+    }
+}
+
+const adminRoute = (req, res, next) => {
+    try{
+        if(req.user && req.user.role === 'admin'){
+            next()
+        }
+        else{
+            return res.status(403).json({
+                success: false,
+                message: "access denied - only admin can perform this operation"
+            })
+        }
+    }
+    catch(error){
+        console.log("error in admin route", error.message)
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,10 +2,12 @@ import express from 'express'
 const router = express.Router()
 
 import { signup, signin, signout, refreshToken, getProfile } from '../controllers/user.controller.js'
+import { protectRoute } from '../middleware/auth.js'
+
 router.post("/signup", signup)
 router.post("/signin", signin)
 router.post("/signout", signout)
 router.post('/refresh-token', refreshToken)
-router.get('/profile', getProfile)
+router.get('/profile',  protectRoute, getProfile)
 
 export default router
